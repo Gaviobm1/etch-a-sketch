6c82b2498c9713664c15b659fdf404bfb6580ac1
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-09-17 19:29:29 +0200

    Add initial UI and functionality

---
+++ etchASketch.js
@@ -0,0 +1,44 @@
+const container = document.querySelector('.container');
+const header = document.querySelector('.header');
+const topButton = document.createElement('button');
+header.appendChild(topButton);
+topButton.textContent = "Click me!"
+
+topButton.addEventListener('click', () => {
+    let userInput = prompt('How long do you want the sides to be?');
+    if (+userInput > 100) { alert("Please enter number 100 or under"); return userInput};
+    let num = +userInput * +userInput; 
+    let sides = 940 / userInput;
+    deletePad ();
+    createDivs(num);
+    Array.from(document.getElementsByClassName('boxy'))
+    .forEach(function (box) {box.style.setProperty ('--max-height', `${sides}px`) });
+    Array.from(document.getElementsByClassName('boxy'));
+    Array.from(document.getElementsByClassName('boxy'))
+    .forEach(function (box) {box.style.setProperty ('--min-width', `${sides}px`) });
+    Array.from(document.getElementsByClassName('boxy'))
+.forEach(function (box) {  box.addEventListener('mouseover', () => box.style.backgroundColor = `#${randomColor()}`)});
+});
+
+
+
+function randomColor () {
+    return Math.floor(Math.random()*16777215).toString(16);
+    }
+
+function createDivs (num) {
+
+    for(let i = 0; i < num; i++) {
+        const box = document.createElement('div');
+        box.classList.add("boxy");
+        container.appendChild(box);
+    }         
+}
+
+function deletePad () {
+Array.from(container.childNodes)
+.forEach(function (box) { container.removeChild(box) } )
+}
+
+
+
